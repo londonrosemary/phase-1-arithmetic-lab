6441b291b7c9c6e0a16ed773f6287742aa10e683
London <london.rosemary@gmail.com>
2021-08-09 18:31:06 -0400

    commit tree 132b6154abcdcfdcebc1b77ca82efb4c04d5111d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 13;
+const num4 = 9;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
